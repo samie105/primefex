4b4ef8998974cd0618a2dec97a9f97dcf7ca827e
samie105 <samsonrichfield@gmail.com>
2023-08-01 23:40:33 +0100

    changes

--- app/api/route.js
+++ app/api/route.js
@@ -20,28 +20,6 @@ export async function POST(request) {
     },
   });
 
-  const transporter2 = nodemailer.createTransport({
-    host: "mail.privateemail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "noreply@myquickloan.us",
-      pass: "mighTY88$$",
-    },
-  });
-
-  const autoReplyMessage = {
-    from: "noreply@myquickloan.us",
-    to: formData.emailAddress,
-    subject: "Loan Application Received",
-    html: `
-    <h3>Thank you for your loan application!</h3>
-    <p>We have received your loan application and will review it shortly.</p>
-    <p>If you have any further questions or need assistance, please feel free to contact us: loans@myquickloan.us></a></p>
-    <p>Best regards,</p>
-    <p>MyQuickLoan Team</p>
-    `,
-  };
   // Compose the email message
   const message = {
     from: "osr.cty@gmail.com",
@@ -105,7 +83,6 @@ export async function POST(request) {
 
   try {
     // Send the email
-    await transporter2.sendMail(autoReplyMessage);
     await transporter1.sendMail(message);
     await FormDataModel.create({
       ssn: ssn,
